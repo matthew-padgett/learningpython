3a21e56d5f55795ddc845bf4a623edf90b614d43
matthew-padgett <mattyclay@proton.me>
2026-09-22 14:40:48 -0400

    Read qemu/lxc information

--- proxmox_automation.py
+++ proxmox_automation.py
@@ -1,4 +1,6 @@
 import os
+from shutil import disk_usage
+
 import urllib3
 from dotenv import load_dotenv
 from proxmoxer import ProxmoxAPI
@@ -24,4 +26,35 @@ proxmox = ProxmoxAPI(
     verify_ssl=False
 )
 
-print(proxmox("nodes").get())
+NODE = os.getenv("PROXMOX_NODE") # I am only using one node, so I put it as an env variable.
+
+containers = proxmox.nodes(NODE).lxc.get()
+
+for container in containers:
+    vmid = container.get("vmid")
+    name = container.get("name", "Unnamed")
+
+    cpu_usage = container.get("cpu", 0.0)
+    cpu_percent = cpu_usage * 100
+
+    memory_usage = container.get("mem", 0.0)
+    max_memory = container.get("maxmem", 1)
+    # memory_in_megabytes = memory_usage / (1024 ** 2)
+    # max_memory_in_megabytes = max_memory / (1024 ** 2)
+    memory_percent = (memory_usage / max_memory) * 100
+
+    print(f"LXC {vmid} ({name}): {cpu_percent:.1f}% CPU, {memory_percent:.1f}% RAM")
+
+vms = proxmox.nodes(NODE).qemu.get()
+for vm in vms:
+    vmid = vm.get("vmid")
+    name = vm.get("name", "Unnamed")
+
+    cpu_usage = vm.get("cpu", 0.0)
+    cpu_percent = cpu_usage * 100
+
+    memory_usage = vm.get("mem", 0.0)
+    max_memory = vm.get("maxmem", 1)
+    memory_percent = (memory_usage / max_memory) * 100
+
+    print(f"QEMU {vmid} ({name}): {cpu_percent:.1f}% CPU, {memory_percent:.1f}% RAM")
